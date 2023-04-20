ccf94fd212cd9a746f0b591785724fb418ff762d
k34r <k34rrr@gmail.com>
2023-04-20 22:51:17 +0700

    modified

--- Pages/MySite.php
+++ Pages/MySite.php
@@ -4,12 +4,13 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>MySite</title>
-	<link rel="stylesheet" type="text/css" href="../CSS/my.css">
+	<link rel="stylesheet" type="text/css" href="../CSS/header.css">
+	<link rel="stylesheet" type="text/css" href="../CSS/MySite.css">
 </head>
 <body>
 <?php
 require_once 'PageComponents/header.html';
 ?>
-
+<div class="background"></div>
 </body>
 </html>
--- Pages/en-ru/en-ru.php
+++ Pages/en-ru/en-ru.php
@@ -4,22 +4,23 @@
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" charset="utf-8">
 	<title>english-russian</title>
+  <link rel="stylesheet" type="text/css" href="./CSS/header.css">
   <link rel="stylesheet" type="text/css" href="./CSS/en-ru.css">
 </head>
 
 <body>
   <?php require_once 'PageComponents/header.html'; ?>
-  <div class="mt-4">
-  <div id="topper" class="container">
+
+<div id="topper" class="container">
    <ul class="list-group" >
      <li id="p1" class="list-group-item">Количество слов в базе: </li>
      <li id="right" class="list-group-item">Правильных ответов: </li>
      <li id="wrong" class="list-group-item">Неправильных ответов: </li>
      <li id="percent" class="list-group-item">Процент правильных ответов: </li>
      <li id="timer" class="list-group-item">Таймер:</li>
-    </ul>
-  </div>
+  </ul>
 </div>
+
 
 <div class="container_user">
   <div class="container_en-word">
